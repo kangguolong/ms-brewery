007f6b42f25534929bb316edd8742c87f05c1426
Peter Kang <kangguolong@gmail.com>
2020-09-27 22:35:15 +1300

    Project architecture

---
+++ src/test/java/com/pk/ms/msbrewery/MsBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.ms.msbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
